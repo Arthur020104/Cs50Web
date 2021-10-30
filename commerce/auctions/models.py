826c48195ac35b27623fb0d0786c1df4d155f8c3
Arthur020104 <arthur020104@gmail.com>
2021-10-30 15:36:39 -0300

    adding final version of commerce project

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     about = models.CharField(max_length=1000)
     creator = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="usernames")
     time_created = models.CharField(max_length=18)
-    category = models.ManyToManyField(Category, blank=True, related_name="categoria")
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, related_name="category")
     status = models.CharField(max_length=8)
     onwatch = models.ManyToManyField(User, blank=True, related_name="buyers")
     img = models.CharField(max_length=10000)
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -4,8 +4,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 import datetime
+from django.contrib.auth.decorators import login_required
 
-from .models import User, Product
+from .models import User, Product, Category
 
 
 def index(request):
@@ -68,23 +69,32 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
+@login_required(redirect_field_name='login')
 def newproduct(request):
-    #must be logged in
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("login"))
+    categoryy = Category.objects.all()
     if request.method == "POST":
-        if not request.POST["productname"] and request.POST["productprice"] and request.POST["productabout"] and request.POST["img"]:
+        if not request.POST["productname"] and request.POST["productprice"] and request.POST["productabout"] and request.POST["img"] and request.POST["Categories"] in (categoryy.name):
             return render(request, "auctions/NewProduct.html", {
-                "message":"All fields must be filled"
+                "message":"All fields must be filled",
+                "Categories":categoryy
             })
         else:
-            p = Product(userwining = request.user,bids_count = 0, current_bid = float(request.POST["productprice"]), creator = request.user, img = request.POST["img"], name = request.POST["productname"], about = request.POST["productabout"], time_created = datetime.date.today(), status = "Active")
-            p.save()
-            return HttpResponseRedirect(reverse("index"))
+            if Category.objects.get(pk = int(request.POST["Categories"])) != [] :
+                p = Product(userwining = request.user,bids_count = 0, current_bid = float(request.POST["productprice"]), creator = request.user, img = request.POST["img"], name = request.POST["productname"], about = request.POST["productabout"], time_created = datetime.date.today(), status = "Active", category = Category.objects.get(pk = int(request.POST["Categories"])))
+                p.save()
+
+                return HttpResponseRedirect(reverse("index"))
+            else:
+                return render(request, "auctions/NewProduct.html", {
+                "message":"Something has gone wrong.",
+                "Categories":categoryy
+            })
         
 
-    return render(request, "auctions/NewProduct.html")
-
+    return render(request, "auctions/NewProduct.html",{
+        "Categories":categoryy
+    })
+@login_required(redirect_field_name='login')
 def product(request, Product_id):
     if request.method == "POST":
         if request.POST["newbid"]:
@@ -110,9 +120,9 @@ def product(request, Product_id):
             })
     else:
         return render(request, "auctions/Product.html", {
-            "Product":Product.objects.get(pk=Product_id)
+            "Product":Product.objects.get(pk=Product_id),
         })
-
+@login_required(redirect_field_name='login')
 def watchlist(request):
     if request.method == "POST":
         product = Product.objects.get(pk=int(request.POST["product_id"]))
