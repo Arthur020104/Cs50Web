c7f2a5904b39d2810abd2effa2e783161396f6e3
Arthur <arthur020104@gmail.com>
2022-04-16 11:04:48 -0300

    movendo o projeto final para um repositorio separado

--- Django-projects/finalproject_cs50web/decidir/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import User,receita, Img
-
-admin.site.register(User)
-admin.site.register(receita)
-admin.site.register(Img)
